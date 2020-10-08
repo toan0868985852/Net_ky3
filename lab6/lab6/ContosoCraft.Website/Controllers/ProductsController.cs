3a6e9f960e094d1a386143266cee029cfc166d37
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-10-09 03:24:42 +0700

    lab6

---
+++ lab6/lab6/ContosoCraft.Website/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCraft.Website.Models;
+using ContosoCraft.Website.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCraft.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsService productsService)
+        {
+            this.ProductsService = productsService;
+        }
+
+        public JsonFileProductsService ProductsService { get;}
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductsService.GetProducts();
+        }
+
+        //[HttpPatch] "[Frombody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductsService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ lab6/lab6/ContosoCraft.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCraft.Website.Models;
+using ContosoCraft.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCraft.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductsService ProductsService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductsService productService)
+        {
+            _logger = logger;
+            ProductsService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductsService.GetProducts();
+        }
+    }
+}
