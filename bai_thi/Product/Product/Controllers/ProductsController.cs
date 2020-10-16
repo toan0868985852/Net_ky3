4edbe54b75747c1348fbfae9b31c92d5f8859855
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-10-16 11:25:14 +0700

    bai_thuc_hanh

---
+++ bai_thi/Product/Product/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product.Models;
+using Product.Services;
+
+namespace Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(ProductServices productService)
+        {
+            ProductService = productService;
+        }
+
+        public ProductServices ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product.Models.Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
--- bai_thi/Product/Product/Startup.cs
+++ bai_thi/Product/Product/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -52,6 +54,12 @@ namespace Product
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapGet("/products", (context) =>
+                {
+                    var products = app.ApplicationServices.GetService<ProductServices>().GetProducts();
+                    var json = JsonSerializer.Serialize<IEnumerable<Product.Models.Product>>(products);
+                    return context.Response.WriteAsync(json);
+                });
             });
         }
     }
