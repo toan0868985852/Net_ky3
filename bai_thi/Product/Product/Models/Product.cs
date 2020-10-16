94932d7f2154acd5af454f482191d1ffefe89e80
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-10-16 10:56:16 +0700

    bai_thi

---
+++ bai_thi/Product/Product/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace Product.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public string image { get; set; }
+        
+        public string price { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
---
+++ bai_thi/Product/Product/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using Product.Services;
+
+namespace Product.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public ProductServices ProductService;
+
+        public IEnumerable<Product.Models.Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, 
+            ProductServices productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
